0f24f58ddb3bed98b9f67a882fe18d71da632f28
Lucas González Ingelmo <lucas.ingelmo@hotmail.com>
2024-12-19 15:28:20 +0100

    Update sql1.php

--- SQL/sql1.php
+++ SQL/sql1.php
@@ -23,11 +23,11 @@
 <?php 
 	$servername = "localhost";
 	$username = "root";
-	$password = "";
+	$password = getenv('MYSQL_SECURE_PASSWORD');
 	$db = "1ccb8097d0e9ce9f154608be60224c7c";
 
 	// Create connection
-	$conn = mysqli_connect($servername,$username,$password,$db);
+	$conn = new mysqli_connect($servername,$username,$password,$db);
 
 	// Check connection
 	if (!$conn) {
